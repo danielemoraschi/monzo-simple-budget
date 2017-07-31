1e8cd48fafad6c1105a0a9973b4558e1c41dec00
Daniele Moraschi <daniele.moraschi@gmail.com>
2017-07-31 18:12:02 +0100

    added status bar

--- src/containers/MonzoApp.js
+++ src/containers/MonzoApp.js
@@ -18,15 +18,18 @@ class HomeScreen extends React.Component {
   render() {
     return (
       <View>
-        <Text>Hello, Monzo APP!</Text>
-        <LoginButton 
-          user_id={'user_00009IhsYAE6t4Z0SmbJab'} 
-          account_id={'acc_00009IhsjMDkdKdgqdlpNh'} 
-          access_token={'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjaSI6Im9hdXRoY2xpZW50XzAwMDA5NFB2SU5ER3pUM2s2dHo4anAiLCJleHAiOjE1MDE1NDIyMzAsImlhdCI6MTUwMTUyMDYzMCwianRpIjoidG9rXzAwMDA5TjBlUWdmUXJxb2h0d3JQUngiLCJ1aSI6InVzZXJfMDAwMDlJaHNZQUU2dDRaMFNtYkphYiIsInYiOiIyIn0.bYz7SV10flRV7hN-sofGUOe-xvCw14AvNRVmfvYHMY4'}
-          title={'Login'}
-        />
-        <LogoutButton title={'Logout'} />
-        <GetBalanceButton title={'Get Balance'} />
+          <StatusBar
+              barStyle="light-content"
+          />
+          <Text>Hello, Monzo APP!</Text>
+          <LoginButton 
+            user_id={'user_00009IhsYAE6t4Z0SmbJab'} 
+            account_id={'acc_00009IhsjMDkdKdgqdlpNh'} 
+            access_token={'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjaSI6Im9hdXRoY2xpZW50XzAwMDA5NFB2SU5ER3pUM2s2dHo4anAiLCJleHAiOjE1MDE1NDIyMzAsImlhdCI6MTUwMTUyMDYzMCwianRpIjoidG9rXzAwMDA5TjBlUWdmUXJxb2h0d3JQUngiLCJ1aSI6InVzZXJfMDAwMDlJaHNZQUU2dDRaMFNtYkphYiIsInYiOiIyIn0.bYz7SV10flRV7hN-sofGUOe-xvCw14AvNRVmfvYHMY4'}
+            title={'Login'}
+          />
+          <LogoutButton title={'Logout'} />
+          <GetBalanceButton title={'Get Balance'} />
       </View>
     );
   }
